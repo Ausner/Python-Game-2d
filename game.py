0b0e8484e349848381e6d337736a8dacb683eb36
Ausner Miranda <ausnermiranda@gmail.com>
2022-11-02 21:28:08 -0600

    Improve the game

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
-from distutils import dir_util
+# from distutils import dir_util
 import pygame
 from pygame.locals import *
-from pyvidplayer import Video
+#from pyvidplayer import Video
 from pygame import gfxdraw
 from random import randint
+from levels.levels import Levels
 
 pygame.init()
 
@@ -18,27 +19,30 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
 
-vid = Video('Tic Tac Toe Video.mp4')
-vid.set_size((screen_width, screen_height))
+#vid = Video('Tic Tac Toe Video.mp4')
+#vid.set_size((screen_width, screen_height))
 
 
-def intro():
-    while True:
-        vid.draw(screen, (0, 0))
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                vid.close()
-                main()
+# def intro():
+#     while True:
+#         vid.draw(screen, (0, 0))
+#         pygame.display.update()
+#         for event in pygame.event.get():
+#             if event.type == pygame.MOUSEBUTTONDOWN:
+#                 vid.close()
+#                 main()
 
 #define game variables
 title_size = 43
 game_over = 0
+main_menu = True
 
 #load images
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png').convert_alpha()
-
+restart_img = pygame.image.load('img/restart.jpg')
+start_img = pygame.image.load('img/start.png')
+exit_img = pygame.image.load('img/exit.png')
 
 class World():
     def __init__(self, data):
@@ -82,27 +86,36 @@ class World():
             #pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
 
-class Player():
-    def __init__(self, x, y):
-        self.images_rigth = []
-        self.images_left = []
-        self.index = 0
-        self.counter = 0
-        for num in range(1, 12):
-            img_rigth = pygame.image.load(f'img/player{num}.png')
-            img_rigth = pygame.transform.scale(img_rigth, (70, 100))
-            img_left = pygame.transform.flip(img_rigth, True, False)
-            self.images_rigth.append(img_rigth)
-            self.images_left.append(img_left)
-        self.image = self.images_rigth[self.index]
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-        self.vel_y = 0
-        self.jumped = False
-        self.direction = 0
+        self.clicked = False
+
+    def draw(self):
+        action = False
+        #mouse position
+        pos = pygame.mouse.get_pos()
+
+        #check mouserover and clicks
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False: #left click
+                action = True
+                self.clicked = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        #draw button
+        screen.blit(self.image, self.rect) 
+        return action
+
+
+class Player():
+    def __init__(self, x, y):
+        self.reset(x, y)
 
     def update(self, game_over):
         dx = 0
@@ -113,7 +126,7 @@ class Player():
         if game_over == 0:
             #get keypresess
             key = pygame.key.get_pressed()
-            if key[pygame.K_SPACE] and self.jumped == False:
+            if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
                 self.vel_y = -15
                 self.jumped = True
 
@@ -156,6 +169,7 @@ class Player():
             dy += self.vel_y
 
             #check for collision
+            self.in_air = True
             for tile in world.tile_list:
                 #check for collision in x directio
                 if tile[1].colliderect(self.rect.x +dx, self.rect.y, self.width, self.height):
@@ -171,6 +185,7 @@ class Player():
                     elif self.vel_y >= 0:
                         dy = tile[1].top - self.rect.bottom
                         self.vel_y = 0
+                        self.in_air = False
 
             #check for collision with enemies
             if pygame.sprite.spritecollide(self, enemies, False):
@@ -188,10 +203,38 @@ class Player():
             self.rect.y += dy
 
 
+        elif game_over == -1:
+            self.image = self.dead_img
+            if self.rect.y >= 200:
+                self.rect.y -= 5
+
         #draw player onto screen
         screen.blit(self.image, self.rect)
         #pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
         return game_over
+    
+    def reset(self, x, y):
+        self.images_rigth = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 12):
+            img_rigth = pygame.image.load(f'img/player{num}.png')
+            img_rigth = pygame.transform.scale(img_rigth, (70, 100))
+            img_left = pygame.transform.flip(img_rigth, True, False)
+            self.images_rigth.append(img_rigth)
+            self.images_left.append(img_left)
+        self.dead_img = pygame.image.load('img/ghost.png')
+        self.image = self.images_rigth[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+        self.in_air = True
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -216,43 +259,32 @@ class Lava(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         image = pygame.image.load('img/lava.png')
-        self.image = pygame.transform.scale(img, (title_size, title_size // 2))
+        self.image = pygame.transform.scale(image, (title_size, title_size // 2))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
 
-world_data = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
-]
+
 
 player = Player(100, screen_height - 130)
 enemies = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
+
+#get the level and create the world
+world_data = Levels.level1
 world = World(world_data)
+
+#buttons
+restar_button = Button(screen_width // 2 - 300, screen_height // 2 -300, restart_img)
+start_button = Button(screen_width // 2  -350, screen_height // 2 - 100, start_img)
+exit_button = Button(screen_width // 2  + 150, screen_height // 2 - 100, exit_img)
+
+
 def main():
 
-
+    game_over = 0
+    main_menu = True
     run = True
 
     while run  == True:
@@ -261,19 +293,36 @@ def main():
         screen.blit(bg_img, (0, 0))
         screen.blit(sun_img, (100, 100))
 
-        world.draw()
-        enemies.update()
-        enemies.draw(screen)
-        
-        lava_group.draw(screen)
 
-        game_over = player.update(game_over)
+        #show main menu
+        if main_menu:
+            if exit_button.draw():
+                run = False
+            if start_button.draw():
+                main_menu = False
+
+        else:
+            world.draw()
+
+            if game_over == 0:
+                enemies.update()
+            enemies.draw(screen)
+            
+            lava_group.draw(screen)
+            game_over = player.update(game_over)
+
+            #if died
+            if game_over == -1:
+                if restar_button.draw():
+                    player.reset(100, screen_height - 130)
+                    game_over = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
-        pygame.display.update()
+            
+            pygame.display.update()
     pygame.quit()
 
-intro()
+main()
+# intro()
